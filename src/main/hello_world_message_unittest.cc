ad5bcbb8d124e1f85a409f5a50e61b7dd3ef586d
Simon <simonfong6@gmail.com>
2021-12-20 00:40:18 -0800

    Empty test

--- src/main/hello_world_message_unittest.cc
+++ src/main/hello_world_message_unittest.cc
@@ -7,3 +7,8 @@ TEST(HelloWorldMessage, HelloWorldMesssageFunc) {
     // Test the message is correct.
     EXPECT_EQ("Hello World!!!", hello_world_message());
 }
+
+TEST(HelloWorldMessage, HelloWorldMesssageFuncNoOp) {
+    // Empty test.
+    EXPECT_EQ("", "");
+}
