61f4a9f476f3ada071d726e1bfcd8e1f1db3d861
Simon <simonfong6@gmail.com>
2021-12-27 00:21:13 -0800

    format

--- src/snake-game-opengl/game.h
+++ src/snake-game-opengl/game.h
@@ -7,9 +7,9 @@
 #define DOWN -1
 #define LEFT -2
 
-void initGrid(int,int);
+void initGrid(int, int);
 void draw_grid();
 void draw_food();
 void draw_snake();
 
-#endif // GAME_H_INCLUDED
+#endif  // GAME_H_INCLUDED
